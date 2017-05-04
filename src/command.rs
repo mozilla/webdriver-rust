3d9f4283a993b55c34cdd6e7f042c09e2a8dd95c
Jason Juang <juangj@google.com>
2017-05-03 18:44:30 -0700

    Fix error messages for timeout params.

--- src/command.rs
+++ src/command.rs
@@ -520,7 +520,7 @@ impl Parameters for TimeoutsParameters {
             Some(json) => {
                 Some(try_opt!(json.as_u64(),
                               ErrorStatus::InvalidArgument,
-                              "Script timeout duration was not a signed integer"))
+                              "Page load timeout duration was not a signed integer"))
             }
             None => None,
         };
@@ -529,7 +529,7 @@ impl Parameters for TimeoutsParameters {
             Some(json) => {
                 Some(try_opt!(json.as_u64(),
                               ErrorStatus::InvalidArgument,
-                              "Script timeout duration was not a signed integer"))
+                              "Implicit timeout duration was not a signed integer"))
             }
             None => None,
         };
